e14ae0f279a3732ba4d1e3fca642e0454c769132
= <timdi@icloud.com>
2019-05-01 16:01:18 +0200

    Added constructors to path ops

--- src/svg/element/shape/MoveTo.java
+++ src/svg/element/shape/MoveTo.java
@@ -1,4 +1,15 @@
 package svg.element.shape;
 
-public class MoveTo {
+import java.util.Scanner;
+
+public class MoveTo extends PathOp {
+    private double x,y;
+
+    public MoveTo(String expr) {
+        String exprPoint1 = expr.substring(1,expr.indexOf(" "));
+        String exprPoint2 = expr.substring(expr.indexOf(" ")+1);
+
+        Scanner scanner = new Scanner(exprPoint1).useDelimiter(",");
+        x = scanner.nextInt(); y = scanner.nextInt();
+    }
 }
