f209c275b3bbe54409184a79e7135f5a23dc26e0
Krzysztof Cybulski <k.cybulski.dev@tuta.io>
2019-05-01 14:28:30 +0200

    Make Shape abstract

--- src/svg/element/shape/Shape.java
+++ src/svg/element/shape/Shape.java
@@ -1,30 +1,10 @@
 package svg.element.shape;
 
 import svg.element.BaseElement;
-import svg.element.Element;
 
-public class Shape extends BaseElement {
+public abstract class Shape extends BaseElement {
 
-    public Shape() {
-        super("Shape");
-    }
-
-    @Override
-    public Element newInstance() {
-        return new Shape();
-    }
-
-    @Override
-    public boolean load(final String expr)
-    {
-        try
-        {
-            throw new Exception("SVG shape loading not implemented yet.");
-        }
-        catch (Exception e)
-        {
-            // ...
-        }
-        return false;
+    public Shape(String label) {
+        super(label);
     }
 }
