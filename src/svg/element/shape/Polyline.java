af2fe6d845d59c274b8d26dd9972584013a61a7d
Nikolas Oikonomou <n.oikonomou@maastrichtuniversity.nl>
2019-05-01 20:41:58 +0200

    Polyline update

--- src/svg/element/shape/Polyline.java
+++ src/svg/element/shape/Polyline.java
@@ -10,9 +10,34 @@ public class Polyline extends Shape {
     List<Point2D> points = new ArrayList<Point2D>();
     double x;
 
-    public boolean load( String expr){
-        
+    public Polyline(String label, List<Point2D> points) {
+        super(label);
+        this.points = points;
     }
+
+    @Override
+    public boolean load(String expr) {
+        try
+        {
+
+            // We first extract the path data string
+            int dataLocation = expr.indexOf("points=") + 2;
+            String dataString = SVGParser.extractStringAt(expr, dataLocation);
+
+            String[] strings = dataString.split(" ");
+
+            for(int i=0; i<strings.length; i = i+2){
+                points.add(new Point(Double.parseDouble(strings[i]), Double.parseDouble(strings[i+1])));
+            }
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
     public Element newInstance(){
         Polyline newPolyline = new Polyline();
         return newPolyline;
