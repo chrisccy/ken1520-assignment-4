80aa72eb9c848b9b12230b8133c0820ef93fd4c9
Krzysztof Cybulski <k.cybulski.dev@tuta.io>
2019-05-01 16:33:00 +0200

    Find Path operation sequences

--- src/svg/element/shape/Path.java
+++ src/svg/element/shape/Path.java
@@ -1,6 +1,10 @@
 package svg.element.shape;
 
+import svg.SVGParser;
 import svg.element.Element;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Path extends Shape {
     public Path() {
@@ -16,12 +20,26 @@ public class Path extends Shape {
     public boolean load(String expr) {
         try
         {
-            throw new Exception("SVG path loading not implemented yet.");
+            // We first extract the path data string
+            int dataLocation = expr.indexOf("d=\"") + 2;
+            String dataString = SVGParser.extractStringAt(expr, dataLocation);
+
+            // Use regex to find operation patterns
+            // [A-Za-z] means "find sequence starting with letter"
+            // [0-9,. ]* means "if there are numbers, dots, commas or spaces after the letter then include them also
+            String patternString = "[A-Za-z][0-9,. ]*";
+            Pattern operationPattern = Pattern.compile(patternString);
+            Matcher match = operationPattern.matcher(dataString);
+
+            while (match.find()) {
+                String opString = match.group();
+                System.out.println(opString);
+            }
+            return true;
         }
         catch (Exception e)
         {
-            // ...
+            return false;
         }
-        return false;
     }
 }
