0cef0fe4227b13385b22843e9e93247d88ad64c3
= <timdi@icloud.com>
2019-05-01 20:04:20 +0200

    Nothing interesting

--- src/svg/SVGParser.java
+++ src/svg/SVGParser.java
@@ -73,36 +73,36 @@ public class SVGParser
 	{	
 		svg.clear();
 
-//		// Load SVG elements
-//		for (Element prototype : BaseElement.elementFactory().prototypes())
-//		{
-//			// Load all occurrences of this prototype
-//			final String label = prototype.label();
-//			
-//			int pos = 0;
-//			while (pos < content.length())
-//			{
-//				pos = content.indexOf("<"+label, pos);
-//				if (pos == -1)
-//					break;
-//
-//				final int to = content.indexOf(">", pos); 
-//				
-//				if (to == -1)
-//				{
-//					System.out.println("* Failed to close expression: " + content.substring(pos));
-//					break;
-//				}
-//				
-//				final String expr = content.substring(pos, to+1);
-//				final Element element = BaseElement.elementFactory().makeElement(label);
-//				element.load(expr);
-//				((BaseElement)element).setFilePos(pos);
-//				svg.elements().add(element);
-//				
-//				pos = to;
-//			}
-//		}
+		// Load SVG elements
+		for (Element prototype : BaseElement.elementFactory().prototypes())
+		{
+			// Load all occurrences of this prototype
+			final String label = prototype.label();
+
+			int pos = 0;
+			while (pos < content.length())
+			{
+				pos = content.indexOf("<"+label, pos);
+				if (pos == -1)
+					break;
+
+				final int to = content.indexOf(">", pos);
+
+				if (to == -1)
+				{
+					System.out.println("* Failed to close expression: " + content.substring(pos));
+					break;
+				}
+
+				final String expr = content.substring(pos, to+1);
+				final Element element = BaseElement.elementFactory().makeElement(label);
+				element.load(expr);
+				((BaseElement)element).setFilePos(pos);
+				svg.elements().add(element);
+
+				pos = to;
+			}
+		}
 
 		sortElements();
 	}
